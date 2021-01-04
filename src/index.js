98659c2263cc7745936eedc5630976e98cbedc64
x1aodingdang <1761689749@qq.com>
2021-01-04 16:11:16 +0800

    feat: 优化分片下载次数

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const http = require("http");
 const path = require("path");
 const child_process = require("child_process");
-const fs = require("fs");
-const log4js = require("log4js");
-const util = require("util");
-const log = require("single-line-log").stdout;
-// const child = require("./child");
 const merge = require("./merge");
 
+const os = require("os");
+
 // const url = "";
-const __URL = "http://192.168.0.167:10000/ubuntu-20.04-desktop-amd64.iso";
+// const __URL = "http://192.168.0.167:10000/ubuntu-20.04-desktop-amd64.iso";
 // const __URL = "http://192.168.0.167:10000/wechat_devtools_1.03.2008270_x64.exe";
+// const __URL = "http://akamaicdn.webex.com/client/webexapp.msi";
+const __URL = "http://file.foxitreader.cn/file/Channel/edit/FoxitInst-R2.exe";
 // const __URL =
 //   "http://gosspublic.alicdn.com/oss-browser/1.7.4/oss-browser-win32-x64.zip?spm=a2c4g.11186623.2.10.5dcc1144IcwSVV&file=oss-browser-win32-x64.zip";
 // const __URL = "http://dl.2345.com/pic/2345pic_v9.3.0.8549.exe";
 // const __URL = "http://mirror.lzu.edu.cn/ubuntu-releases/20.04.1/ubuntu-20.04.1-desktop-amd64.iso";
 // const __URL =
-//   "http://onlinedown.rbread04.cn/huajuns afe/VMware-workstation-full-16.0.0-16894299.exe";
+//   "http://onlinedown.rbread04.cn/huajunsafe/VMware-workstation-full-16.0.0-16894299.exe";
 // const __URL =
 //   "http://forspeed.rbread05.cn/down/newdown/10/22/baofengjihuogonji_v17.0.rar";
 // const __URL =
@@ -25,7 +24,7 @@ const __URL = "http://192.168.0.167:10000/ubuntu-20.04-desktop-amd64.iso";
 // const OUTPUT = path.join(__dirname, `output`);
 const OUTPUT = path.join(path.resolve(__dirname, "../"), `output`);
 
-const COUNT = 100;
+const COUNT = os.cpus().length;
 
 // child_process.exec("rm -rf ./output/")
 
@@ -33,10 +32,7 @@ const COUNT = 100;
 http
   .get(__URL, {}, (res, req) => {
     const { statusCode, headers, url } = res;
-    // 2715254784   2651616
-    // console.log("statusCode: ", statusCode);
-    // console.log("headers: ", headers);
-    // console.log("url: ", url, headers, headers.path);
+
     const ip = res.socket.localAddress;
     const port = res.socket.localPort;
     const _path = (res.connection._httpMessage.path || "").split("?")[0] || "";
@@ -103,18 +99,7 @@ http
         // console.log("params", params);
         // child(params);
 
-        const child = child_process.fork(
-          `./src/child.js`,
-          [],
-          {}
-          // (error, stdout, stderr) => {
-          //   console.log(error, stdout, stderr);
-          //   if (error) {
-          //     throw error;
-          //   }
-          //   console.log(stdout);
-          // }
-        );
+        const child = child_process.fork(`./src/child.js`, [], {});
 
         child.on("message", (message) => {
           if (message.type === "CB") {
@@ -157,33 +142,6 @@ http
     } else {
       console.log("statusCode", statusCode);
     }
-
-    // return;
-
-    // res.on("data", (chunk) => {
-    //   // console.log("chunk", chunk);
-    //   size += chunk.length;
-
-    //   chunks.push(chunk);
-    //   const cmdText = `Progress ---${size}/${Len} \n`;
-    //   log(cmdText);
-
-    //   // console.log(`Progress ---${size}/${Len}`);
-
-    //   // Do nothing
-    // });
-    // res.on("end", () => {
-    //   try {
-    //     const buf = Buffer.concat(chunks, size);
-
-    //     // console.log(buf.toString());
-    //     fs.writeFile(path.join(OUTPUT, filename), buf, {}, (res) => {
-    //       console.log(res);
-    //     });
-    //   } catch (error) {
-    //     console.error("end-error", error);
-    //   }
-    // });
   })
   .on("error", (error) => {
     console.log("http on error", error.code);
